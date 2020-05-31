6a9a82fd0478cda0c602e812497eb3e4230ab10e
msyphus <mark.syphus@gmail.com>
2020-05-30 22:14:56 -0600

    added JAWSDB connection for Heroku deployment

--- config/connection.js
+++ config/connection.js
@@ -1,12 +1,16 @@
 var mysql = require("mysql");
 var sqlpass = require("../key");
+var connection;
 
-var connection = mysql.createConnection({ 
-    host: "localhost",
-    port: 3308,
-    user: "root",
-    password: sqlpass,
-    database: "orders_db"
+if (process.env.JAWSDB_URL) {
+    connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else { 
+    connection = mysql.createConnection({
+        host: "localhost",
+        port: 3308,
+        user: "root",
+        password: sqlpass,
+        database: "orders_db"
 });
 
 connection.connect(function(err) {
